19d9f37c39bacf23fc3eaac0918782f335ba5498
AnuwatBunsong <fdog216@gmail.com>
2023-04-26 10:10:51 +0700

    Add The service file

---
+++ services/department_service.js
@@ -0,0 +1,45 @@
+import { prisma } from "../prismaClient";
+
+export const DepartmentService = {
+    createDepartment: async (data) => {
+      try {
+        return await prisma.department.create({ data });
+      } catch (error) {
+        console.error("Error creating department:", error);
+        throw error;
+      }
+    },
+    getAllDepartments: async () => {
+      try {
+        return await prisma.department.findMany();
+      } catch (error) {
+        console.error("Error fetching departments:", error);
+        throw error;
+      }
+    },
+    getDepartmentById: async (id) => {
+      try {
+        return await prisma.department.findUnique({ where: { id } });
+      } catch (error) {
+        console.error(`Error fetching department with ID ${id}:`, error);
+        throw error;
+      }
+    },
+    updateDepartment: async (id, data) => {
+      try {
+        return await prisma.department.update({ where: { id }, data });
+      } catch (error) {
+        console.error(`Error updating department with ID ${id}:`, error);
+        throw error;
+      }
+    },
+    deleteDepartment: async (id) => {
+      try {
+        return await prisma.department.delete({ where: { id } });
+      } catch (error) {
+        console.error(`Error deleting department with ID ${id}:`, error);
+        throw error;
+      }
+    },
+  };
+  
--- services/ministry_service.js
+++ services/ministry_service.js
@@ -1,23 +1,45 @@
-import { prisma } from "../prismaClient";
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 
 export const MinistryService = {
-    create: async (data) => {
+  createMinistry: async (data) => {
+    try {
       return await prisma.ministry.create({ data });
-    },
-  
-    findAll: async () => {
+    } catch (error) {
+      console.error("Error creating ministry:", error);
+      throw error;
+    }
+  },
+  getAllMinistries: async () => {
+    try {
       return await prisma.ministry.findMany();
-    },
-  
-    findById: async (id) => {
+    } catch (error) {
+      console.error("Error fetching ministries:", error);
+      throw error;
+    }
+  },
+  getMinistryById: async (id) => {
+    try {
       return await prisma.ministry.findUnique({ where: { id } });
-    },
-  
-    update: async (id, data) => {
+    } catch (error) {
+      console.error(`Error fetching ministry with ID ${id}:`, error);
+      throw error;
+    }
+  },
+  updateMinistry: async (id, data) => {
+    try {
       return await prisma.ministry.update({ where: { id }, data });
-    },
-  
-    delete: async (id) => {
+    } catch (error) {
+      console.error(`Error updating ministry with ID ${id}:`, error);
+      throw error;
+    }
+  },
+  deleteMinistry: async (id) => {
+    try {
       return await prisma.ministry.delete({ where: { id } });
-    },
-  };
+    } catch (error) {
+      console.error(`Error deleting ministry with ID ${id}:`, error);
+      throw error;
+    }
+  },
+};
